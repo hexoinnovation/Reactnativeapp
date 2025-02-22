fb8cf4f3f7b89c104ca4da14f9d17f23914f260d
Nithyavs <hexoinnovation@gmail.com>
2025-02-22 11:26:13 -0800

    Nithya

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -7,16 +7,10 @@ export default function Dashboard() {
 
   return (
     <View style={styles.container}>
-      {/* Navbar (Header) */}
-      {/* <View style={styles.navbar}> */}
-        {/* <Button title="☰ Menu" onPress={() => navigation.openDrawer()} /> */}
-       
-      {/* </View> */}
-
       {/* Main Content */}
       <View style={styles.content}>
-        <Text style={styles.text}>Welcome to your Dashboard!</Text>
-        <Button title="Logout" onPress={() => navigation.navigate('StartScreen')} />
+       
+       
       </View>
     </View>
   );
@@ -25,15 +19,6 @@ export default function Dashboard() {
 // ✅ Styles for Layout
 const styles = StyleSheet.create({
   container: { flex: 1, backgroundColor: '#f5f5f5' },
-  navbar: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    height: 60,
-    paddingHorizontal: 15,
-  },
-  title: { color: '#fff', fontSize: 18, fontWeight: 'bold' },
   content: { flex: 1, justifyContent: 'center', alignItems: 'center' },
   text: { fontSize: 20, marginBottom: 10 },
 });
-
